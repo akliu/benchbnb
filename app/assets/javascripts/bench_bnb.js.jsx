912098f7c6bfb1d83514ae938e618c372e0271b5
akliu <andrew@Andrews-MacBook-Pro.local>
2015-10-12 10:44:17 -0700

    React Router working

---
+++ app/assets/javascripts/bench_bnb.js.jsx
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  $(function(){
+    var root = document.getElementById('content');
+    var Route = ReactRouter.Route;
+    var Router = ReactRouter.Router;
+    var IndexRoute = ReactRouter.IndexRoute;
+
+    var App = React.createClass({
+      render: function(){
+        return (
+          <div>
+            <header><h1>Bench BnB</h1></header>
+            {this.props.children}
+          </div>
+        );
+      }
+    });
+
+    var routes = (
+      <Route path="/" component={App}>
+        <IndexRoute component={Search} />
+      </Route>
+    );
+
+    React.render(<Router>{routes}</Router>, root);
+
+  });
+
+  // $(function(){
+  //   React.render(
+  //     <Search />, document.getElementById("content")
+  //   );
+  //
+  // });
+
+}());
